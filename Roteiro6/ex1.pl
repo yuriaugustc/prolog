0414d23db3ce634c99f04912e89b376589622732
Yuri Augusto da Costa <yuri.costa@sedoc.local>
2022-01-24 17:22:24 -0300

    roteiro6

---
+++ Roteiro6/ex1.pl
@@ -0,0 +1,53 @@
+ordinal --> s.
+ordinal(A):-
+	
+
+s --> sn.
+
+sn --> n1.
+sn --> n2.
+sn --> n3.
+sn --> n4.
+sn --> n2, n1.
+sn --> n3, n2.
+sn --> n3, n1.
+sn --> n3, n2, n1.
+
+
+n1 --> [primeiro].
+n1 --> [segundo].
+n1 --> [terceiro].
+n1 --> [quarto].
+n1 --> [quinto].
+n1 --> [sexto].
+n1 --> [setimo].
+n1 --> [oitavo].
+n1 --> [nono].
+
+n2 --> [decimo].
+n2 --> [vingesimo].
+n2 --> [trigesimo].
+n2 --> [quadragesimo].
+n2 --> [quinquagesimo].
+n2 --> [sextuagesimo].
+n2 --> [septuagesimo].
+n2 --> [setuagesimo].
+n2 --> [octagesimo].
+n2 --> [nonagesimo].
+
+n3 --> [centesimo].
+n3 --> [ducentesimo].
+n3 --> [trecentesimo].
+n3 --> [tricentesimo].
+n3 --> [quadrigentesimo].
+n3 --> [quingentesimo].
+n3 --> [sexcentesimo].
+n3 --> [seiscentesimo].
+n3 --> [septingentesimo].
+n3 --> [setingentesimo].
+n3 --> [octingentesimo].
+n3 --> [noningentesimo].
+n3 --> [nongentesimo].
+
+n4 --> [milesimo].
+
---
+++ Roteiro6/ex5.pl
@@ -0,0 +1,11 @@
+teste([
+<, !, 'DOCTYPE', html, >,
+<, html, lang, =, 'pt-BR', >,
+<, head, >,
+<, meta, charset, =, 'UTF-8', >,
+<, title, >, 'TItulo', da, 'Pagina', 'Basica', <, /, title, >,
+<, /, head, >,
+<, body, >,
+<, /, body, >,
+<, /, html, >
+]).
